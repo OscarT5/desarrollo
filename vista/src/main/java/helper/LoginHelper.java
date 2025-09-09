e1e996191ef55e364e574d14e0d7fe0ac358bbe5
Oscar Tan Guan <oscar.tan@uabc.edu.mx>
2025-09-09 19:15:16 +0100

    Revert "Branch new feature"

--- vista/src/main/java/helper/LoginHelper.java
+++ vista/src/main/java/helper/LoginHelper.java
@@ -12,7 +12,7 @@ import java.io.Serializable;
 
 public class LoginHelper implements Serializable {
     
- //cambioooooooooajksdfakfjas lfas flas jfla lkfalksfdlas jlfals
+ //cambiojkdfasljkflañksaflksj
     /**
      * Metodo para hacer login llamara a la instancia de usuarioFacade
      * @param correo
