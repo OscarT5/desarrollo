1063448058890ea08565e44b227e02a2bcb62229
Oscar Tan Guan <oscar.tan@uabc.edu.mx>
2025-09-09 18:57:15 +0100

    Branch new feature

--- vista/src/main/java/helper/LoginHelper.java
+++ vista/src/main/java/helper/LoginHelper.java
@@ -12,7 +12,7 @@ import java.io.Serializable;
 
 public class LoginHelper implements Serializable {
     
- //cambio
+ //cambiooooooooo
     /**
      * Metodo para hacer login llamara a la instancia de usuarioFacade
      * @param correo
